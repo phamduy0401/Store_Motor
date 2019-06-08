a6edd51c29f9dbfcfed391bc8373016f1193b845
TienTM <tienit04021997@gmail.com>
2019-06-09 00:11:38 +0700

    update

--- index.php
+++ index.php
@@ -1,2 +1,133 @@
+<?php
+require_once __DIR__."/autoload/autoload.php";
+?>
 
-<h3>Đồ án tốt nghiệp</h3>
+<?php require_once __DIR__."/layouts/header.php"; ?>
+<div class="col-md-9 bor">
+    <section id="slide" class="text-center" >
+        <img src="images/slide/sl3.jpg" class="img-thumbnail">
+    </section>
+
+    <section class="box-main1">
+        <h3 class="title-main" style="text-align: center;"><a href="javascript:void(0)"> Máy Canong </a> </h3>
+
+        <div class="showitem">
+            <div class="col-md-3 item-product bor">
+                <a href="">
+                    <img src="images/anh1.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-3  item-product bor">
+                <a href="">
+                    <img src="images/anh2.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-3  item-product bor">
+                <a href="">
+                    <img src="images/anh4.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-3  item-product bor">
+                <a href="">
+                    <img src="images/anh3.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+
+            <div class="col-md-3 item-product bor">
+                <a href="">
+                    <img src="images/anh1.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-3  item-product bor">
+                <a href="">
+                    <img src="images/anh2.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-3  item-product bor">
+                <a href="">
+                    <img src="images/anh4.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-3  item-product bor">
+                <a href="">
+                    <img src="images/anh3.png" class="" width="100%" height="180">
+                </a>
+                <div class="info-item">
+                    <a href="">Lorem ipsum dolor sit amet</a>
+                    <p><strike class="sale">19.000.000 đ</strike> <b class="price">11.000.000 đ</b></p>
+                </div>
+                <div class="hidenitem">
+                    <p><a href=""><i class="fa fa-search"></i></a></p>
+                    <p><a href=""><i class="fa fa-heart"></i></a></p>
+                    <p><a href=""><i class="fa fa-shopping-basket"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php require_once __DIR__."/layouts/footer.php"; ?>
+
+
